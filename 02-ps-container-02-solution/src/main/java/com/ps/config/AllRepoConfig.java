988b50d77e13126dfc68c3cb988a084f6d5aa96c
Iuliana Cosmina <iuliana.cosmina@gmail.com>
2016-03-31 08:06:58 +0300

    16 started annotations

---
+++ 02-ps-container-02-solution/src/main/java/com/ps/config/AllRepoConfig.java
@@ -0,0 +1,26 @@
+package com.ps.config;
+
+import com.ps.repos.PetRepo;
+import com.ps.repos.impl.JdbcPetRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by iuliana.cosmina on 3/21/16.
+ * Sample class to depict how @ImportResource can be used
+ */
+@Configuration
+public class AllRepoConfig {
+
+
+    @Autowired
+    DataSource dataSource;
+
+    @Bean
+    public PetRepo petRepo() {
+        return new JdbcPetRepo(dataSource);
+    }
+}
---
+++ 02-ps-container-02-solution/src/test/java/com/ps/BootstrapTest.java
@@ -0,0 +1,7 @@
+package com.ps;
+
+/**
+ * Created by iuliana.cosmina on 3/31/16.
+ */
+public class BootstrapTest {
+}
