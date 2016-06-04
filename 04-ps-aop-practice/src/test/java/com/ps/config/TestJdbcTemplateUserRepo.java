4fb085c74c8848638eb49b1e0543198f85709449
Iuliana Cosmina <iuliana.grajdeanu@bearingpoint.com>
2016-06-04 22:46:06 +0300

    22 added more AOP examples

---
+++ 04-ps-aop-practice/src/test/java/com/ps/config/TestJdbcTemplateUserRepo.java
@@ -0,0 +1,66 @@
+package com.ps.config;
+
+import com.ps.ents.User;
+import com.ps.repos.UserRepo;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by iuliana.cosmina on 6/4/16.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {TestDataConfig.class, AppConfig.class})
+@ActiveProfiles("dev")
+public class TestJdbcTemplateUserRepo {
+
+    @Autowired
+    @Qualifier("userTemplateRepo")
+    UserRepo userRepo;
+
+    @Before
+    public void setUp() {
+        assertNotNull(userRepo);
+    }
+
+    @Test
+    public void testOne() {
+        Set<User> result = userRepo.findAllByUserName("John", true);
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    public void testFindById() {
+        User user = userRepo.findById(1L);
+        assertEquals("John", user.getUsername());
+    }
+
+    @Test
+    public void testUpdatePass() {
+        userRepo.updatePassword(1L, "new_pass");
+        User user = userRepo.findById(1L);
+        assertEquals("new_pass", user.getPassword());
+    }
+
+    @Test
+    public void testMore() {
+        Set<User> result = userRepo.findAllByUserName("John", false);
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    public void testAll() {
+        Set<User> result = userRepo.findAll();
+        assertEquals(4, result.size());
+    }
+}
--- 04-ps-aop-practice/src/test/java/com/ps/config/TestJdbcUserRepo.java
+++ 04-ps-aop-practice/src/test/java/com/ps/config/TestJdbcUserRepo.java
@@ -2,12 +2,11 @@ package com.ps.config;
 
 import com.ps.ents.User;
 import com.ps.repos.UserRepo;
-import com.ps.repos.impl.JdbcUserRepo;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -21,48 +20,47 @@ import static org.junit.Assert.assertNotNull;
  * Created by iuliana.cosmina on 6/1/16.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {TestDataConfig.class, AppConfig.class, AspectConfig.class})
+@ContextConfiguration(classes = {TestDataConfig.class, AppConfig.class})
 @ActiveProfiles("dev")
-public class TestUserRepo {
+public class TestJdbcUserRepo {
 
     @Autowired
+    @Qualifier("userRepo")
     UserRepo userRepo;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         assertNotNull(userRepo);
     }
 
-
     @Test
-    public void testOne(){
-        Set<User> result =  userRepo.findAllByUserName("John", true);
+    public void testOne() {
+        Set<User> result = userRepo.findAllByUserName("John", true);
         assertEquals(1, result.size());
     }
 
     @Test
-    public void testFindById(){
-        User user =  userRepo.findById(1L);
+    public void testFindById() {
+        User user = userRepo.findById(1L);
         assertEquals("John", user.getUsername());
     }
 
     @Test
-    public void testUpdatePass(){
+    public void testUpdatePass() {
         userRepo.updatePassword(1L, "new_pass");
-        User user =  userRepo.findById(1L);
+        User user = userRepo.findById(1L);
         assertEquals("new_pass", user.getPassword());
     }
 
-
     @Test
-    public void testMore(){
-        Set<User> result =  userRepo.findAllByUserName("John", false);
+    public void testMore() {
+        Set<User> result = userRepo.findAllByUserName("John", false);
         assertEquals(2, result.size());
     }
 
     @Test
-    public void testAll(){
-        Set<User> result =  userRepo.findAll();
-        assertEquals(3, result.size());
+    public void testAll() {
+        Set<User> result = userRepo.findAll();
+        assertEquals(4, result.size());
     }
 }
