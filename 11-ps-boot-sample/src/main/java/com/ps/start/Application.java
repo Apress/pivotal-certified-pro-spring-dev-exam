905e631ddc204dfadd065471f10aca0771e1420a
Iuliana Cosmina <iuliana.cosmina@gmail.com>
2016-08-23 21:04:18 +0300

    55 added TomcatEmbeddedServletContainerFactory

--- 11-ps-boot-sample/src/main/java/com/ps/start/Application.java
+++ 11-ps-boot-sample/src/main/java/com/ps/start/Application.java
@@ -1,7 +1,13 @@
 package com.ps.start;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by iuliana.cosmina on 8/22/16.
@@ -12,5 +18,22 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("Started ...");
+    }
+
+
+    @Value("${app.port}")
+    private Integer value;
+
+    @Value("${app.context}")
+    private String contextPath;
+
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+        factory.setPort(value);
+        factory.setSessionTimeout(10, TimeUnit.MINUTES);
+        factory.setContextPath(contextPath);
+        return factory;
     }
 }
--- 11-ps-boot-sample/src/main/java/com/ps/start/YamlConfBean.java
+++ 11-ps-boot-sample/src/main/java/com/ps/start/YamlConfBean.java
@@ -1,4 +1,4 @@
-package com.ps.init;
+package com.ps.start;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
@@ -8,17 +8,17 @@ import org.springframework.stereotype.Component;
 /**
  * Created by iuliana.cosmina on 9/27/15.
  */
-@Component
+//@Component
 public class YamlConfBean implements EmbeddedServletContainerCustomizer {
 
-    @Autowired
-    private AppSettings appSettings;
+   // @Autowired
+   // private AppSettings appSettings;
 
 
     @Override
     public void customize(ConfigurableEmbeddedServletContainer container) {
-        container.setPort(appSettings.getPort());
-        container.setContextPath(appSettings.getContext());
+       // container.setPort(appSettings.getPort());
+       // container.setContextPath(appSettings.getContext());
     }
 }
 
