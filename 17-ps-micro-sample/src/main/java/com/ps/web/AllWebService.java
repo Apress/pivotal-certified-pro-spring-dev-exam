958bdb4bca584fdff44823fecc6993794fe5e0ea
Iuliana Cosmina <iuliana.cosmina@gmail.com>
2016-10-27 18:56:50 +0300

    74 added final version for microservices, with test links

--- 17-ps-micro-sample/src/main/java/com/ps/web/AllWebService.java
+++ 17-ps-micro-sample/src/main/java/com/ps/web/AllWebService.java
@@ -1,5 +1,6 @@
 package com.ps.web;
 
+import com.ps.ex.UserNotFoundException;
 import com.ps.pet.Pet;
 import com.ps.user.User;
 import org.slf4j.Logger;
@@ -7,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -27,14 +29,29 @@ public class AllWebService {
 
 	public UserSkeleton findUserById(Long id) {
 		logger.info("findUserById(" + id + " ) called");
-		User user = restTemplate.getForObject(usersServiceUrl + "/users/id/{id}", User.class, id);
+		User user = null;
+		try {
+			 user = restTemplate.getForObject(usersServiceUrl + "/users/id/{id}", User.class, id);
+		}catch (HttpClientErrorException e) {
+			// no user
+			throw new UserNotFoundException(id);
+		}
 		return new UserSkeleton(user.getId(), user.getUsername());
 	}
 
 	public List<PetSkeleton> findByOwnerId(Long ownerId) {
 		logger.info("findByOwnerId(" + ownerId + " ) called");
-		Pet[] pets = restTemplate.getForObject(petsServiceUrl + "/pets/owner/{id}",
-				com.ps.pet.Pet[].class, ownerId);
+		Pet[] pets = null;
+		try {
+			pets = restTemplate.getForObject(petsServiceUrl + "/pets/owner/{id}",
+					com.ps.pet.Pet[].class, ownerId);
+		} catch (HttpClientErrorException e) {
+			// no pets
+		}
+
+		if(pets == null || pets.length ==0) {
+			return null;
+		}
 		List<PetSkeleton> petsList = new ArrayList<>();
 		for (Pet pet : pets) {
 			petsList.add(new PetSkeleton(pet.getName(), pet.getAge(), pet.getPetType().toString()));
